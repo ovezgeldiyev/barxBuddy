1c0a9d2dd7bb5c76d9b30debdda9e68597642d2f
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2023-12-06 10:12:57 +0500

    asd

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// sliders
+$(function () {
+  $(".introReview__slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    speed: 600,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
+
